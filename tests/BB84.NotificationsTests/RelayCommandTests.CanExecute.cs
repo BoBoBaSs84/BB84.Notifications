6c4bce4f33873ee0505411c411a5b1477b4af86c
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-01-08 23:42:22 +0100

    feat: relay command implementation
    changes:
    - the `IRelayCommand` interface added
    - the `RelayCommand` class added
    - unit tests added
    
    closes #55

---
+++ tests/BB84.NotificationsTests/RelayCommandTests.CanExecute.cs
@@ -0,0 +1,27 @@
+﻿namespace BB84.NotificationsTests;
+
+public sealed partial class RelayCommandTests
+{
+  [TestMethod]
+  public void CanExecute()
+  {
+    TestClass test;
+
+    test = new TestClass();
+
+    Assert.IsTrue(test.Command.CanExecute(this));
+    Assert.IsFalse(test.CondCommand.CanExecute(this));
+  }
+
+  [TestMethod]
+  public void CanExecuteWithParam()
+  {
+    TestClass test;
+
+    test = new TestClass();
+
+    Assert.IsTrue(test.IntCommand.CanExecute(1));
+    Assert.IsTrue(test.IntCommand.CanExecute((object)1));
+    Assert.IsFalse(test.CondIntCommand.CanExecute(1));
+  }
+}
---
+++ tests/BB84.NotificationsTests/RelayCommandTests.Execute.cs
@@ -0,0 +1,25 @@
+﻿namespace BB84.NotificationsTests;
+
+public sealed partial class RelayCommandTests
+{
+  [TestMethod]
+  public void Execute()
+  {
+    TestClass test = new();
+
+    test.Command.Execute(this);
+
+    Assert.AreEqual(1, test.CommandExecution);
+  }
+
+  [TestMethod]
+  public void ExecuteWithParam()
+  {
+    TestClass test = new();
+
+    test.IntCommand.Execute((object)1);
+    test.IntCommand.Execute(2);
+
+    Assert.AreEqual(2, test.IntegerCommandExecution);
+  }
+}
