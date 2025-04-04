5966a78edd4a0dea8dc51f93c86a7aba38d955f3
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2025-04-04 22:02:34 +0200

    feat: add license header

--- src/BB84.Notifications/Interfaces/Commands/IActionCommand.cs
+++ src/BB84.Notifications/Interfaces/Commands/IActionCommand.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Input;
+﻿// Copyright: 2023 Robert Peter Meyer
+// License: MIT
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+using System.Windows.Input;
 
 namespace BB84.Notifications.Interfaces.Commands;
 
