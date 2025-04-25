dceef261c9f973ecbec0aa553bc20a4d7e4a297d
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2025-04-25 16:08:34 +0200

    feat: implicit operator for the notifiable property unit tests added

---
+++ tests/BB84.NotificationsTests/NotifiablePropertyTests.ImplicitOperators.cs
@@ -0,0 +1,26 @@
+﻿using BB84.Notifications;
+
+namespace BB84.NotificationsTests;
+
+public sealed partial class NotifiablePropertyTests
+{
+  [TestMethod]
+  public void ConvertValueToValueTypeTest()
+  {
+    int value = 42;
+
+    NotifiableProperty<int> notifiableProperty = value;
+
+    Assert.AreEqual(value, notifiableProperty.Value);
+  }
+
+  [TestMethod]
+  public void ConvertValueTypeToValueTest()
+  {
+    NotifiableProperty<int> notifiableProperty = new(42);
+
+    int value = notifiableProperty;
+
+    Assert.AreEqual(notifiableProperty.Value, value);
+  }
+}
