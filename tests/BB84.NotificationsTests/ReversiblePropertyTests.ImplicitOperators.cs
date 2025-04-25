e765c4a89f96d7cfdcb85c2ddbbcd9574a701094
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2025-04-25 16:13:02 +0200

    feat: implicit operator for the reversible property and unit tests added

---
+++ tests/BB84.NotificationsTests/ReversiblePropertyTests.ImplicitOperators.cs
@@ -0,0 +1,26 @@
+﻿using BB84.Notifications;
+
+namespace BB84.NotificationsTests;
+
+public sealed partial class ReversiblePropertyTests
+{
+  [TestMethod]
+  public void ConvertValueToValueTypeTest()
+  {
+    int value = 42;
+
+    ReversibleProperty<int> notifiableProperty = value;
+
+    Assert.AreEqual(value, notifiableProperty.Value);
+  }
+
+  [TestMethod]
+  public void ConvertValueTypeToValueTest()
+  {
+    ReversibleProperty<int> notifiableProperty = new(42);
+
+    int value = notifiableProperty;
+
+    Assert.AreEqual(notifiableProperty.Value, value);
+  }
+}
